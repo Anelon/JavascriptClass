6326a79539dfb456a8dcf90a3f56307f8649d394
Andrew Bell <majacow@gmail.com>
2016-01-14 04:32:46 +0000

    End of day 2 commit

---
+++ day1/app.js
@@ -0,0 +1,22 @@
+//Declair vars
+var firstName = "Andrew";
+var lastName = "Bell";
+var fullName;
+var numArr = [2, 4, 6];
+var average;
+
+//part 1
+fullName = firstName + " " + lastName;
+console.log(fullName);
+
+//part 2
+var fourteen = 2*(4 + 3);
+console.log(fourteen);
+
+//part 3
+average = (numArr[0] + numArr[1] + numArr[2]) / numArr.length;
+console.log(numArr[0],numArr[1],numArr[2]);
+console.log(average);
+
+//success?
+console.log("it's working");
---
+++ day2/app.js
@@ -0,0 +1,5 @@
+var firstName = prompt("What is your first name?");
+console.log(firstName);
+var lastName = prompt("What is your  last name?");
+alert(lastName);
+confirm("This is you?");
